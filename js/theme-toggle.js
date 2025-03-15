55e9760b2b691481df5c2bc7a4dc865d35e513fd
ronaldanom <67653271+ronaldanom@users.noreply.github.com>
2025-03-15 00:20:50 -0500

    Update theme-toggle.js

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,48 +1,42 @@
-// theme-toggle.js
-
-// Lista de los temas disponibles
+// Definir los temas disponibles
 const themes = ['jekyll-theme-slate', 'jekyll-theme-dark', 'jekyll-theme-tactile', 'jekyll-theme-midnight'];
 
 // Función para cambiar entre los temas
 function changeTheme() {
-  // Obtener el índice del tema actual almacenado en localStorage
+  // Obtener el índice del tema actual del localStorage
   let currentThemeIndex = localStorage.getItem("currentThemeIndex");
-  
-  // Si no hay índice guardado, inicializarlo en 0 (primer tema)
+
+  // Si no hay un tema guardado, iniciar con el primer tema
+  if (currentThemeIndex === null) {
+    currentThemeIndex = 0; // Comienza con el primer tema
+  } else {
+    currentThemeIndex = parseInt(currentThemeIndex); // Convertir a número
+  }
+
+  // Incrementar el índice para cambiar al siguiente tema
+  currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+
+  // Establecer el tema seleccionado en localStorage
+  localStorage.setItem("currentThemeIndex", currentThemeIndex);
+
+  // Remover todas las clases de los temas anteriores
+  document.body.classList.remove(...themes);
+
+  // Agregar el nuevo tema al body
+  document.body.classList.add(themes[currentThemeIndex]);
+}
+
+// Al cargar la página, aplicar el tema guardado en localStorage
+window.onload = function() {
+  let currentThemeIndex = localStorage.getItem("currentThemeIndex");
+
+  // Si no hay un tema guardado, iniciar con el primer tema
   if (currentThemeIndex === null) {
     currentThemeIndex = 0;
   } else {
     currentThemeIndex = parseInt(currentThemeIndex); // Convertir a número
   }
 
-  // Incrementar el índice y asegurarse de que no se salga del rango
-  currentThemeIndex = (currentThemeIndex + 1) % themes.length;
-
-  // Obtener el nuevo tema basado en el índice
-  let newTheme = themes[currentThemeIndex];
-
-  // Guardar el índice del nuevo tema en localStorage
-  localStorage.setItem("currentThemeIndex", currentThemeIndex);
-
-  // Remover las clases de los temas anteriores
-  document.body.classList.remove(...themes);
-
-  // Agregar la clase del nuevo tema
-  document.body.classList.add(newTheme);
-}
-
-// Al cargar la página, establecer el tema previamente seleccionado
-window.onload = function() {
-  // Obtener el índice del tema guardado
-  let currentThemeIndex = localStorage.getItem("currentThemeIndex");
-
-  // Si no hay un índice guardado, iniciar con el primer tema
-  if (currentThemeIndex === null) {
-    currentThemeIndex = 0;
-  } else {
-    currentThemeIndex = parseInt(currentThemeIndex);
-  }
-
-  // Agregar la clase correspondiente al tema
+  // Agregar el tema guardado al body
   document.body.classList.add(themes[currentThemeIndex]);
 };
